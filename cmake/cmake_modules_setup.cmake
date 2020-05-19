7b13bbdca4194db14c52d112ef1fc5bbb4151ce1
Andrey Ponomarev <andrey.ponomarev.1408@gmail.com>
2020-05-19 15:51:20 +0300

    Deleted m.css from repo(will be fetched now if documentation is enabled)
    deleted submodules

--- cmake/cmake_modules_setup.cmake
+++ cmake/cmake_modules_setup.cmake
@@ -10,6 +10,12 @@
 # \brief
 # Cmake file for setting up module paths
 
+include(FetchContent)
+FetchContent_Declare(
+        googletest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG        release-1.8.0
+)
 
 set(CMAKE_MODULE_PATH 
     ${CMAKE_CURRENT_SOURCE_DIR}/cmake
