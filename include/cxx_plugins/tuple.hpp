495c978c242c63711003da88488cb2f45ac91652
Andrey Ponomarev <andrey.ponomarev.1408@gmail.com>
2020-05-22 15:12:06 +0300

    Added TupleMap class
    Finished documenting TupleMap
    
    Removed vtable from the project, as TupleMap is more generic and can be used instead
    
    Fixing non clang compilers
    
    Fixed windows compiler
    
    Updated indexing of types to be non recursive
    
    Adopted non recursive indexing.
    
    checking weird stuff on other platforms
    
    quick fix for CMakeLists.txt
    
    another quick fix

--- include/cxx_plugins/tuple.hpp
+++ include/cxx_plugins/tuple.hpp
@@ -16,6 +16,8 @@
  */
 #pragma once
 
+#include "cxx_plugins/type_traits.hpp"
+
 #include <tuple>
 
 namespace CxxPlugins {
@@ -28,17 +30,217 @@ namespace CxxPlugins {
  */
 template <typename... TArgs> using Tuple = std::tuple<TArgs...>;
 
-//! Alias for std::tuple_element
-template <size_t I, typename T>
-using TupleElement = typename std::tuple_element<I, T>::type;
+template <std::size_t I, typename T> struct TupleElement;
+
+template <std::size_t I, typename... Ts> struct TupleElement<I, Tuple<Ts...>> {
+  using Type = utility::ElementType<I, Ts...>;
+};
+//! Alias for TupleElement::Type
+template <std::size_t I, typename T>
+using TupleElementType = typename TupleElement<I, T>::Type;
 
 //! Alias for std::get(<tuple>)
-template <size_t id, typename T> static constexpr auto get(T &&val) -> auto & {
+template <std::size_t id, typename T>
+static constexpr auto get(T &&val) -> auto & {
   return std::get<id>(std::forward<T>(val));
 }
 
 //! Alias for std::get(<tuple>)
-template <size_t id, typename T> static constexpr auto get(T &val) -> auto & {
+template <std::size_t id, typename T>
+static constexpr auto get(T &val) -> auto & {
   return std::get<id>(val);
 }
+
+template <typename... Tuples> constexpr auto tupleCat(Tuples &&... args) {
+  return std::tuple_cat(std::forward<Tuples>(args)...);
+}
+
+template <typename... T>
+using TupleCatType = decltype(tupleCat(std::declval<T>()...));
+
+template <typename T, typename U> using Pair = std::pair<T, U>;
+
+template <typename T, typename U> constexpr auto makePair(T &&t, U &&u) {
+  return Pair<std::decay_t<T>, std::decay_t<U>>{std::forward<T>(t),
+                                                std::forward<U>(u)};
+}
+
+template <typename... Types>
+constexpr auto forwardAsTuple(Types &&... args) noexcept -> Tuple<Types &&...> {
+  return std::forward_as_tuple(std::forward<Types>(args)...);
+}
+
+template <typename T> struct TupleSize;
+
+template <typename... Types> struct TupleSize<Tuple<Types...>> {
+  static constexpr std::size_t value = sizeof...(Types);
+};
+
+template <typename T>
+static constexpr std::size_t tuple_size_v = TupleSize<T>::value;
+
+template <class F, class Tuple>
+constexpr decltype(auto) apply(F &&f, Tuple &&t) {
+  return std::apply(std::forward<F>(f), std::forward<Tuple>(t));
+}
+
+namespace impl {
+
+template <typename... TArgs>
+using AlignmentsOf = std::integer_sequence<std::size_t, alignof(TArgs)...>;
+
+template <typename... TArgs>
+using SizesOf = std::integer_sequence<std::size_t, sizeof(TArgs)...>;
+
+template <std::size_t CurrentOffset, typename T>
+using RequiredOffset =
+    std::integral_constant<std::size_t,
+                           (CurrentOffset % alignof(T) == 0
+                                ? CurrentOffset
+                                : (CurrentOffset / alignof(T) + 1) *
+                                      alignof(T))>; // round up
+
+/*
+ * First, Second, Third
+ *
+ * size = required_offset<required_offset<sizeof(First), Second> +
+ * sizeof(Second), Third>;
+ *
+ *
+ *
+ */
+
+template <std::size_t current, typename T>
+static constexpr std::size_t my_offset = RequiredOffset<current, T>::value +
+                                         sizeof(T);
+
+template <std::size_t I, typename Seq> struct SequenceAt;
+
+template <std::size_t I, typename T, T... values>
+struct SequenceAt<I, std::integer_sequence<T, values...>> {
+  using Type = std::integral_constant<
+      T,
+      TupleElementType<I, Tuple<std::integral_constant<T, values>...>>::value>;
+};
+
+template<std::size_t I, typename Seq>
+using SequenceTypeAt = typename SequenceAt<I,Seq>::Type;
+
+
+template <typename T, T... values, T to_append>
+constexpr auto append(std::integer_sequence<T, values...> /*unused*/,
+                      std::integral_constant<T, to_append> /*unused*/) {
+  return std::integer_sequence<T, values..., to_append>{};
+}
+
+template <typename T, T... values, T... to_append>
+constexpr auto append(std::integer_sequence<T, values...> /*unused*/,
+                      std::integer_sequence<T, to_append...> /*unused*/) {
+  return std::integer_sequence<T, values..., to_append...>{};
+}
+
+template <typename... TArgs> struct AlignedTupleStorageSize {
+  using sizes = SizesOf<TArgs...>;
+  using alignments = AlignmentsOf<TArgs...>;
+};
+
+template <std::size_t previous_offset, typename... TArgs> struct AlignedOffsets;
+
+template <std::size_t previous_offset, typename CurrentType, typename... Rest>
+struct AlignedOffsets<previous_offset, CurrentType, Rest...> {
+
+  static constexpr std::size_t current_offset =
+      RequiredOffset<previous_offset, CurrentType>::value;
+
+  using Type = decltype(
+      append(std::integer_sequence<std::size_t, current_offset>{},
+             typename AlignedOffsets<current_offset + sizeof(CurrentType),
+                                     Rest...>::Type{}));
+};
+
+template <std::size_t previous_offset> struct AlignedOffsets<previous_offset> {
+  using Type = std::integer_sequence<std::size_t>;
+};
+
+template <typename... TArgs>
+using AlignedOffsetsSequence = typename AlignedOffsets<0, TArgs...>::Type;
+
+template <std::size_t I, typename T, T... values>
+constexpr auto sequenceAt(std::integer_sequence<T, values...> /*unused*/) {
+  using tuple = Tuple<std::integral_constant<T, values>...>;
+  return TupleElementType<I, tuple>{};
+}
+
+template <typename... TArgs> struct PackedTupleStorage {
+
+  using Offsets = AlignedOffsetsSequence<TArgs...>;
+  using Sizes = SizesOf<TArgs...>;
+
+  static constexpr std::size_t size =
+     SequenceTypeAt<sizeof...(TArgs) - 1, Offsets>::value +
+     SequenceTypeAt<sizeof...(TArgs) - 1, Sizes>::value;
+
+  alignas(alignof(utility::ElementType<0, TArgs...>)) char data_m[size];
+};
+
+template<> struct PackedTupleStorage<> {
+  using Offsets = std::integer_sequence<std::size_t>;
+  using Sizes = std::integer_sequence<std::size_t>;
+  static constexpr std::size_t size = 0;
+
+  char data_m[size];
+};
+
+
+struct primitive{
+  char data_m[0];
+};
+
+static_assert(sizeof(primitive) == 0,"a");
+
+struct simple {
+  int i;
+  int j;
+  int k;
+};
+
+static_assert(sizeof(simple) == sizeof(PackedTupleStorage<int, int, int>),
+              "Sizes are different");
+
+struct more_complex {
+  int i;
+  char ch;
+  int j;
+};
+
+ static_assert(sizeof(more_complex) == sizeof(simple),
+              "more_complex and simple are of different size");
+ static_assert(sizeof(PackedTupleStorage<int, char, int>) ==
+                  sizeof(more_complex),
+              "Sizes are different");
+
+struct worst {
+  double d;
+  char ch;
+  int i;
+  int j;
+  double d1;
+};
+
+ static_assert(sizeof(worst) ==
+                  sizeof(PackedTupleStorage<double, char, int, int, double>),
+              "dough");
+ static_assert(alignof(worst) ==
+                  alignof(PackedTupleStorage<double, char, int, int, double>),
+              "dough");
+
+} // namespace impl
+
+template <typename... Ts>
+struct PackedTuple {
+public:
+private:
+//  impl::PackedTupleStorage<Ts...>
+};
+
 } // namespace CxxPlugins
--- include/cxx_plugins/type_traits.hpp
+++ include/cxx_plugins/type_traits.hpp
@@ -14,28 +14,76 @@
 #pragma once
 
 #include <type_traits>
+#include <utility>
 
 //! \brief Contains helper functions/classes
 namespace CxxPlugins::utility {
-template <typename T, typename... Ts> struct Index;
-
-template <typename T> struct Index<T> {
-  static_assert(sizeof(T) != 0, "Index is out of bounds");
-};
-
-template <typename T, typename... Ts>
-struct Index<T, T, Ts...> : std::integral_constant<std::size_t, 0> {};
-
-template <typename T, typename U, typename... Ts>
-struct Index<T, U, Ts...>
-    : std::integral_constant<std::size_t, 1 + Index<T, Ts...>::value> {};
 
 template <typename... Types> struct AreUnique;
 
 template <typename First, typename... Rest> struct AreUnique<First, Rest...> {
   static constexpr bool value = (!std::is_same_v<First, Rest> && ...);
 };
+template <> struct AreUnique<> { static constexpr bool value = true; };
 
 template <typename... Types>
 static constexpr bool are_unique_v = AreUnique<Types...>::value;
+
+template <typename T, typename... U> struct IsInThePack {
+  static constexpr bool value = (std::is_same_v<T, U> || ...);
+};
+
+template <typename T, typename... U>
+static constexpr bool is_in_the_pack_v = IsInThePack<T, U...>::value;
+
+namespace impl {
+template <typename T, std::size_t I> struct IndexStorage {
+  using Type = T;
+  static constexpr std::size_t value = I;
+};
+
+template <typename Is, typename... Ts> struct Indexer;
+
+template <std::size_t... Is, typename... Ts>
+struct Indexer<std::integer_sequence<std::size_t, Is...>, Ts...>
+    : IndexStorage<Ts, Is>... {
+};
+
+template <typename T, std::size_t I>
+static constexpr auto select(IndexStorage<T, I>) -> IndexStorage<T, I>;
+
+template<std::size_t I, typename T>
+static constexpr auto select(IndexStorage<T,I>)->IndexStorage<T,I>;
+
+} // namespace impl
+
+template <typename T, typename... Ts>
+static constexpr std::size_t index_of = decltype(impl::select<T>(
+    impl::Indexer<std::make_integer_sequence<std::size_t, sizeof...(Ts)>,
+                  Ts...>{}))::value;
+template<std::size_t I, typename... Ts>
+using ElementType = typename decltype(impl::select<I>(
+    impl::Indexer<std::make_integer_sequence<std::size_t, sizeof...(Ts)>,
+        Ts...>{}))::Type;
+
+template <typename LeftSequence, typename RightSequence>
+struct IntegerSequenceCat;
+
+template <typename T, T... left_indices, T... right_indices>
+struct IntegerSequenceCat<std::integer_sequence<T, left_indices...>,
+                          std::integer_sequence<T, right_indices...>> {
+  using Type = std::integer_sequence<T, left_indices..., right_indices...>;
+};
+
+template <typename T, T value, std::size_t n> struct IntegerSequenceRepeat {
+  static_assert(n > 0, "n should be greater than 0");
+  using Type = typename IntegerSequenceCat<
+      std::integer_sequence<T, value>,
+      typename IntegerSequenceRepeat<T, value, n - 1>::Type>::Type;
+};
+
+template <typename T, T value> struct IntegerSequenceRepeat<T, value, 0> {
+  using Type = std::make_integer_sequence<T, 0>;
+};
+
 } // namespace CxxPlugins::utility
