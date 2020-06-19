89f951df1f30032ae294435dd5d324b6c145f3bf
Timur <timur.kazh@gmail.com>
2020-06-19 02:00:35 +0000

    didn't actually commit my changes last time.

--- include/cxx_plugins/polymorphic.hpp
+++ include/cxx_plugins/polymorphic.hpp
@@ -15,12 +15,58 @@
 #pragma once
 
 #include "cxx_plugins/vtable.hpp"
+#include "cxx_plugins/function_proxy.hpp"
 #include "cxx_plugins/polymorphic_traits.hpp"
 
-namespace impl {
-  template<typename Allocator, >
-  class PolymorphicImpl {
-    template<typename T, typename... Tags>
-    PolymorphicImpl(T&& t, Tags tags...)
+namespace CxxPlugins {
+  
+  template<typename Allocator, typename... Tags, typename... FunctionSignatures>
+  class Polymorphic {
+    template <typename U>
+    static constexpr bool is_same = std::is_same_v<std::decay_t<U>, PolymorphicRef>;
+
+    static constexpr bool is_const = (utility::FunctionTraits<FunctionSignatures>::is_const && ...);
+
+    template <typename T> struct underlying;
+  
+    template <typename T> struct underlying<T const &> {
+      using type = T const &;
+    };
+    template <typename T> struct underlying<T &> {
+      using type = std::conditional_t<is_const, T const &, T &>;
+    };
+    template <typename T> struct underlying<T &&> {
+      using type = std::conditional_t<is_const, T const &, T &&>;
+    };
+    template <typename T> struct underlying<T const &&> {
+      using type = T const &&;
+    };
+  
+    template <typename T> using underlying_t = typename underlying<T>::type;
+    
+    VtableT<TaggedValue<Tags, FunctionSignatures>...> *function_table_p_m = nullptr;
+    Allocator allocator_m;
+    std::conditional_t<is_const, void const*, void*> obj_m;
+  public:
+    template<typename T, typename = std::enable_if_t<!is_same<T>>>
+    constexpr Polymorphic(T&& t) noexcept :  function_table_p_m(&vtable_v<underlying_t<decltype(t)>, TaggedValue<Tags, FunctionSignatures>...>) {
+      obj_m = allocator_m.allocate(sizeof(T)).ptr;
+      new (obj_m) std::decay_t<T>(std::forward<T>(t));    
+    }
+    
+    ~Polymorphic() {
+      allocator_m.deallocate(obj_m);
+      obj_m = nullptr;
+    }
+    
+    template <typename TagT> constexpr auto operator[](TagT&& t) noexcept {
+      return FunctionProxy((*function_table_p_m)[std::forward<TagT>(t)], data_p_m);
+    }
+
+    template <typename TagT> constexpr auto operator[](TagT&& t) const noexcept {
+      return FunctionProxy((*function_table_p_m)[std::forward<TagT>(t)],
+                         const_cast<void const *>(data_p_m));
+    }
   };
-}
+  
+} // namespace CxxPlugins
